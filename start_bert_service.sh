4f9d5112a434a794e00549f582aa6de6631253a3
Muhammad Irfan <virtuoso.irfan@gmail.com>
2020-03-30 15:43:18 +0500

    proper path.

--- start_bert_service.sh
+++ start_bert_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source .bert/bin/activate
-bert-serving-start -model_dir=bert_model/uncased_L-2_H-128_A-2 -num_worker=1 -max_seq_len=20
+bert-serving-start -model_dir=bert/wwm_uncased_L-24_H-1024_A-16 -num_worker=1 -max_seq_len=20
