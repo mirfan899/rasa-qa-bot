e8592c36c3f8658ca5830055f663986a55c8e97b
Muhammad Irfan <virtuoso.irfan@gmail.com>
2020-03-30 15:41:23 +0500

    nohup removed.

--- start_bert_service.sh
+++ start_bert_service.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source .bert/bin/activate
-nohup bert-serving-start -model_dir=bert_model/uncased_L-2_H-128_A-2 -num_worker=1 -max_seq_len=20 > /dev/null 2>&1 &
+bert-serving-start -model_dir=bert_model/uncased_L-2_H-128_A-2 -num_worker=1 -max_seq_len=20
