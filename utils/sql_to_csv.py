861feb75a83c8b7218aa463e4a2981723b5a565a
Muhammad Irfan <virtuoso.irfan@gmail.com>
2020-04-08 23:11:51 +0500

    clean code.

--- utils/sql_to_csv.py
+++ utils/sql_to_csv.py
@@ -1,12 +1,6 @@
 import mysql.connector
 from textblob import TextBlob
 
-# Usin service account of My Project on GCP
-# path = "zippy-nexus-116320-1adad038b7e5.json"
-# os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path
-# from google.cloud import translate_v2 as translate
-# translate_client = translate.Client()
-# Connect to server
 cnx = mysql.connector.connect(
     host="127.0.0.1",
     port=3306,
@@ -41,13 +35,10 @@ for row in rows:
 # Close connection
 cnx.close()
 for q, a in zip(questions, answers):
-    # detector = Detector(q)
-    # google language translate api for detecting language.
     b = TextBlob(q)
     print('Text: {}'.format(q))
     print("From TextBlob =================================================")
     print(b.detect_language())
-    # print(detector.language)
     # print("Question: {}".format(q))
     # print("Answer: {}".format(a))
 
