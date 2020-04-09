824fbde77753d9559f2d764a6556b561e3435eaa
Muhammad Irfan <virtuoso.irfan@gmail.com>
2020-04-09 11:45:44 +0500

    lang detection using gcp api

--- utils/sql_to_csv.py
+++ utils/sql_to_csv.py
@@ -1,44 +1,44 @@
 import mysql.connector
-from textblob import TextBlob
+import os
+import pandas as pd
+from google.cloud import translate_v2 as translate
+# Usin service account of My Project on GCP
+path = "filename.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = path
 
+translate_client = translate.Client()
+# Connect to server
 cnx = mysql.connector.connect(
     host="127.0.0.1",
     port=3306,
     user="root",
-    password="@pp!fy",
+    password="password",
     database='Questions')
 
 # Get a cursor
 cur = cnx.cursor()
 
 # Execute a query
-cur.execute("SELECT * from QUESTIONS_AND_ANSWERS limit 10")
+cur.execute("SELECT * from QUESTIONS_AND_ANSWERS")
 
 # Fetch one result
 # print(cur.description)
 questions = []
 answers = []
-
-# fetch 5 records
-rows = cur.fetchmany(5)
+rows = cur.fetchall()
 for row in rows:
-    questions.append(row[1])
-    answers.append(row[2])
-
-# fetch next 5 records
-rows = cur.fetchmany(5)
-for row in rows:
-    questions.append(row[1])
-    answers.append(row[2])
-
+    q = translate_client.detect_language(row[1])
+    a = translate_client.detect_language(row[2])
+    if q["language"] == 'en' and a["language"] == 'en':
+        questions.append(row[1])
+        answers.append(row[2])
 
 # Close connection
 cnx.close()
-for q, a in zip(questions, answers):
-    b = TextBlob(q)
-    print('Text: {}'.format(q))
-    print("From TextBlob =================================================")
-    print(b.detect_language())
-    # print("Question: {}".format(q))
-    # print("Answer: {}".format(a))
 
+data = pd.DataFrame({"questions": questions, "answers": answers})
+print(data.head(5))
+data.dropna(inplace=True)
+data.drop_duplicates(inplace=True)
+
+data.to_csv("english_questions_answers.csv", index=False)
