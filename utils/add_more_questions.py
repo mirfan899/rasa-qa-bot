92f8becdd39f0813236b2de31e87fac91289d9ea
Muhammad Irfan <virtuoso.irfan@gmail.com>
2020-04-11 12:42:11 +0500

    key issues.

--- utils/add_more_questions.py
+++ utils/add_more_questions.py
@@ -1,7 +1,11 @@
 # encoding: utf-8
 import json
 qa = json.load(open("english_questions_answers.json", encoding="utf-8"))
-questions = [question["q"].strip() for question in qa]
+questions = []
+for k, v in qa.items():
+    if k is "q":
+        print(v)
+        questions.append(v)
 
 md = open("data/nlu/qa.md", "a")
 for question in questions:
