a0ecd5b2fec2e473d49c2fc5aa4466ef0d51a315
Muhammad Irfan <virtuoso.irfan@gmail.com>
2020-04-11 12:47:55 +0500

    list of dicts

--- utils/add_more_questions.py
+++ utils/add_more_questions.py
@@ -2,10 +2,10 @@
 import json
 qa = json.load(open("english_questions_answers.json", encoding="utf-8"))
 questions = []
-for k, v in qa.items():
-    if k is "q":
-        print(v)
-        questions.append(v)
+for dic in qa:
+    for k, v in qa.items():
+        if k is "q":
+            questions.append(v)
 
 md = open("data/nlu/qa.md", "a")
 for question in questions:
