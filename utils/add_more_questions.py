9747661907b242923b912c1feddb22ec958c9dd9
Muhammad Irfan <virtuoso.irfan@gmail.com>
2020-04-11 12:18:48 +0500

    ..

--- utils/add_more_questions.py
+++ utils/add_more_questions.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 import json
-questions = json.load(open("english_questions_answers.json", encoding="utf-8"))
-questions = [question["q"].strip() for question in questions]
+qa = json.load(open("english_questions_answers.json", encoding="utf-8"))
+questions = [question["q"].strip() for question in qa]
 
 md = open("data/nlu/qa.md", "a")
 for question in questions:
