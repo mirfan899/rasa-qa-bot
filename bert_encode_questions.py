5c3d6f295f4960e53d2106aa945a123bf940377f
Muhammad Irfan <virtuoso.irfan@gmail.com>
2020-04-14 13:06:59 +0500

    use pandas

--- bert_encode_questions.py
+++ bert_encode_questions.py
@@ -1,15 +1,16 @@
 from bert_serving.client import BertClient
 import numpy as np
 import json
+import pandas as pd
 
 
 def encode_standard_question():
     bc = BertClient(check_length=False)
-    data = json.load(open("./data/nlu/qa.json", "r", encoding="utf-8"))
-    standard_questions = [each['q'] for each in data]
+    data = pd.read_json("./data/nlu/qa.json")
+    standard_questions = data["q"].tolist()
     print("Standard question size", len(standard_questions))
     print("Start to calculate encoder....")
-    standard_questions_encoder = bc.encode_async(standard_questions)
+    standard_questions_encoder = bc.encode(standard_questions)
     np.save("./data/standard_questions", standard_questions_encoder)
     standard_questions_encoder_len = np.sqrt(np.sum(standard_questions_encoder * standard_questions_encoder, axis=1))
     np.save("./data/standard_questions_len", standard_questions_encoder_len)
